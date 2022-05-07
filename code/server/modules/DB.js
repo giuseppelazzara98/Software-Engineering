91f10f01525a774104fb4516a6eeedf823ff4173
giuseppelazzara98 <s302064@studenti.polito.it>
2022-05-07 22:14:00 +0200

    add SKUItem and DB Class

---
+++ code/server/modules/DB.js
@@ -0,0 +1,118 @@
+class DB{
+  sqlite = require('sqlite3');
+  constructor(dbname) {
+      this.db = new this.sqlite.Database(dbname, (err) => {
+          if(err) throw err;
+      });
+      
+  }
+
+  //SKUITEM
+  deleteSKUItem(rfid) {
+      return new Promise((resolve, reject)  => {
+          const sql = 'DELETE FROM TABLE WHERE RFID=?';//WRITE SQL COMMAND 
+          this.db.run(sql,[rfid], (err) => {
+              if (err) {
+                  reject(err);
+                  return;
+              }
+              resolve(this.lastID);
+          });
+      });
+  }
+
+
+  newTableName() {//MODIFY THIS FUNCTION
+      return new Promise((resolve, reject)  => {
+          const sql = 'CREATE TABLE IF NOT EXISTS NAMES(ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME VARCHAR, SURNAME VARCHAR)';
+          this.db.run(sql, (err) => {
+              if (err) {
+                  reject(err);
+                  return;
+              }
+              resolve(this.lastID);
+          });
+      });
+  }
+
+
+
+  postSkuItem(data) {
+      return new Promise((resolve, reject) => {
+          const sql = 'INSERT INTO TABLENAME(RFID,SKUId,DateOfStock,) VALUES(?, ?, ?)';//WRITE SQL COMMAND
+          this.db.run(sql, [data.RFID, data.SKUId, data.DateOfStock], (err) => {
+              if (err) {
+                reject(err);
+                return;
+              }
+              resolve(this.lastID);
+          });
+      });
+  }
+
+
+  getSKUItems() {
+    return new Promise((resolve, reject) => {
+        const sql = '';//WRITE SQL COMMAND
+        this.db.all(sql, [], (err, rows) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            const skuitems = rows.map((r) => (
+                {  
+                   RFID:r.RFID,
+                   SKUId:r.SKUId,
+                   Available:r.Available,
+                   DateOfStock:r.DateOfStock
+                }
+            ));
+            resolve(skuitems);
+        });
+    });
+}
+
+getSKUItemsID(id) {
+  return new Promise((resolve, reject) => {
+      const sql = 'SELECT * FROM WHERE id=?';//WRITE SQL COMMAND
+      this.db.all(sql, [id], (err, rows) => {
+          if (err) {
+              reject(err);
+              return;
+          }
+          const skuitemsID = rows.map((r) => (
+              {  
+                 RFID:r.RFID,
+                 SKUId:r.SKUId,
+                 DateOfStock:r.DateOfStock
+              }
+          ));
+          resolve(skuitemsID);
+      });
+  });
+}
+
+getSKUItemsRFID(rfid) {
+  return new Promise((resolve, reject) => {
+      const sql = 'SELECT * FROM WHERE RFID=?';//WRITE SQL COMMAND
+      this.db.all(sql, [rfid], (err, rows) => {
+          if (err) {
+              reject(err);
+              return;
+          }
+          const skuitemsRFID = rows.map((r) => (
+              {  
+                 RFID:r.RFID,
+                 SKUId:r.SKUId,
+                 Available:r.Available,
+                 DateOfStock:r.DateOfStock
+              }
+          ));
+          resolve(skuitemsRFID);
+      });
+  });
+}
+
+}
+
+module.exports = DB;
--- code/server/server.js
+++ code/server/server.js
@@ -1,18 +1,68 @@
 'use strict';
+import './modules/DB'
+import DB from './modules/DB';
 const express = require('express');
 // init express
 const app = new express();
 const port = 3001;
 
 app.use(express.json());
+const db=new DB;
 
-//GET /api/test
-app.get('/api/hello', (req,res)=>{
-  let message = {
-    message: 'Hello World!'
+//SKUITEM
+app.get('/api/skuitems', async (req,res) => {
+  try {
+    const skuitems = await db.getSKUItems();
+    res.status(200).json(skuitems);
+  } catch (err) {
+    res.status(404).end();
   }
-  return res.status(200).json(message);
 });
+app.get('/api/skuitems:id', async (req,res) => {
+  try {
+    const id= req.params.id;
+    const skuitems = await db.getSKUItemsID(id);
+    res.status(200).json(skuitems);
+  } catch (err) {
+    res.status(404).end();
+  }
+});
+app.get('/api/skuitems:rfid', async (req,res) => {
+  try {
+    const rfid= req.params.rfid;
+    const skuitems = await db.getSKUItemsRFID(rfid);
+    res.status(200).json(skuitems);
+  } catch (err) {
+    res.status(404).end();
+  }
+});
+
+app.post('/api/skuitem', async (req,res) => {
+  if (Object.keys(req.body).length === 0) {
+    return res.status(422).json({error: `Empty body request`});
+  }
+  let skuitem = req.body.skuitem;
+  if (skuitem === undefined || skuitem.RFID === undefined || skuitem.SKUId === undefined ) {
+    return res.status(422).json({error: `Invalid user data`});//ADD CHECK FOR DATA
+  }
+  await db.newTableName();//MODIFY THIS FUNCTION 
+  db.postSkuItem(skuitem);
+  return res.status(201).end();
+});
+
+app.delete('/api/skuitems/:rfid', (req,res) => {
+  try {
+    const rfid= req.params.rfid;
+    db.deleteSKUItem(rfid);
+    res.status(204).end();
+  } catch (err) {
+    res.status(503).end();
+  }
+});
+
+
+
+
 
 // activate the server
 app.listen(port, () => {
