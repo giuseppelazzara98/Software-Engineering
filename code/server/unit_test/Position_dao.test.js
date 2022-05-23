a14ff36e60b4affaa250a5b8b6a1d7c9290337ff
farzad imanpour <imanpourfarzad@gmail.com>
2022-05-23 10:28:53 +0200

    add position test file

---
+++ code/server/unit_test/Position_dao.test.js
@@ -0,0 +1,17 @@
+const Position_dao = require("../modules/dao/Position_dao");
+
+describe("Test isolate functions", () => {
+  test("test convertResultSetToDomainModelposition", () => {
+    // todo
+  });
+  test("tes validatePositionID", () => {
+    //todo
+  });
+  test("buildPositionID", () => {
+    //todo
+  });
+});
+
+describe("Test dao functions", () => {
+  test("test getAllPositions", async () => {});
+});
